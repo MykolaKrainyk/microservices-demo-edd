1679d3eb924b2843c51d3ec5cc37b7cd27df64ef
Mykola Krainyk <mykola_krainyk@epam.com>
2023-11-25 10:59:34 +0200

    add logger and kafka keywords config

--- twitter-to-kafka-service/src/main/java/com/microservices/demo/twitter/to/kafka/service/TwitterToKafkaServiceApplication.java
+++ twitter-to-kafka-service/src/main/java/com/microservices/demo/twitter/to/kafka/service/TwitterToKafkaServiceApplication.java
@@ -1,13 +1,22 @@
 package com.microservices.demo.twitter.to.kafka.service;
 
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationListener;
+
+import javax.annotation.PostConstruct;
 
 @SpringBootApplication
-public class TwitterToKafkaServiceApplication {
+public class TwitterToKafkaServiceApplication implements CommandLineRunner {
 
     public static void main(String[] args) {
         SpringApplication.run(TwitterToKafkaServiceApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("App starts...");
     }
 }
